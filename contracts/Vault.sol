3fd30da47ab336a4fc0ad8e4e13b58f31b20035b
Gurleen Kaur <gurleen.kaur@antiersolutions.com>
2022-10-31 11:22:19 +0000

    updated Vault

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -4,17 +4,19 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/utils/ERC721HolderUpgradeable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "hardhat/console.sol";
+
 import "Interfaces/IUSDT.sol";
 
-contract vault is ERC20Upgradeable, ERC721HolderUpgradeable, Ownable {
+contract vault is ERC20Upgradeable, ERC721HolderUpgradeable, OwnableUpgradeable {
     address public admin;
-    address public usdt;
+    IUSDT public usdt;
     address public token721;
     uint256 public fractionPrice;
     uint256 public tokenID;
-    uint256 public fractionSupply = totalSupply();
+    uint256 public fractionSupply;
     uint256 private offerNumber = 1;
     bool primaryBuyEnd;
     struct offer {
@@ -33,35 +35,37 @@ contract vault is ERC20Upgradeable, ERC721HolderUpgradeable, Ownable {
         uint256 _fractionPrice,
         address _usdt,
         address _admin
-    ) external initializer {
+    ) external initializer{
+        __Ownable_init();
         require(_admin != address(0), "ZAA"); //Zero address for admin
         require(_token721 != address(0), "ZAT"); //Zero address for token
         __ERC721Holder_init();
-        __ERC20_init(_name, _symbol);
+        __ERC20_init_unchained(_name, _symbol);
         admin = _admin;
         token721 = _token721;
-        usdt = _usdt;
+        usdt = IUSDT(_usdt);
         fractionPrice = _fractionPrice;
         tokenID = _tokenID;
         mint(address(this), _tokenSupply);
+        fractionSupply = totalSupply();
     }
 
     function mint(address _to, uint256 _amount) internal {
-        // require(totalSupply()+_amount<=tokenSupply,"IA");//Invalid amount
+      //  require(totalSupply()+_amount<=tokenSupply,"IA");//Invalid amount
         _mint(_to, _amount);
     }
 
     function buyFractions(uint256 _fractionAmount) external {
-        require(!primaryBuyEnd, "AFS"); //All Fractions Sold
-        require(fractionSupply >= _fractionAmount, "NES"); //Not Enough Supply
-        IUSDT(usdt).transfer(
-            msg.sender,
-            owner(),
-            _fractionAmount * fractionPrice
-        );
+        require(!primaryBuyEnd,"AFS");//All Fractions Sold
+        require(fractionSupply >=_fractionAmount,"NES");//Not Enough Supply 
+        IUSDT(usdt)._transferFrom(msg.sender, owner(), _fractionAmount*fractionPrice);
+
         _transfer(address(this), msg.sender, _fractionAmount);
-        fractionSupply = fractionSupply - _fractionAmount;
-        if (fractionSupply == 0) primaryBuyEnd = true;
+
+        fractionSupply =fractionSupply - _fractionAmount;
+        if(fractionSupply==0)
+            primaryBuyEnd = true;
+        
     }
 
     function transfer(address _to, uint256 _amount)
@@ -70,16 +74,19 @@ contract vault is ERC20Upgradeable, ERC721HolderUpgradeable, Ownable {
         onlyOwner
         returns (bool)
     {
+
         require(_to != address(0), "ZA"); //zero address
         _transfer(address(this), _to, _amount);
         return true;
     }
-
+// for make offer are we taking fraction price of nft price?
+// can multiple people offer same amount?
     function makeOffer(uint256 offerredPrice) external {
         require(primaryBuyEnd, "NAY"); //Not Available Yet
         require(offerredPrice > fractionPrice, "PL"); //Price too low
         uint256 priceToPay = offerredPrice * totalSupply();
-        IERC20Upgradeable(usdt).transfer(address(this), priceToPay);
+        IUSDT(usdt)._transferFrom(msg.sender,address(this), priceToPay);
+       console.log("vault balance",usdt.balanceOf(address(this)));
         offerredAmounts[offerNumber] = offer(
             msg.sender,
             offerredPrice,
@@ -88,20 +95,21 @@ contract vault is ERC20Upgradeable, ERC721HolderUpgradeable, Ownable {
         offerNumber++;
     }
 
-    function voteOffer(uint256 _offerNumber, bool vote) external {
+      function voteOffer(uint256 _offerNumber, bool vote) external {
         require(balanceOf(msg.sender) > 0, "NF"); //No Fractions
         require(msg.sender != offerredAmounts[_offerNumber].offerrer, "ONA"); //Offerrer not allowed
-        if (vote == true) {
-            IERC20Upgradeable(usdt).transferFrom(
+        if (vote == true) {  
+            IUSDT(usdt).transfer(
                 address(this),
                 msg.sender,
                 (balanceOf(msg.sender) * offerredAmounts[_offerNumber].offerred)
             );
-            transferFrom(
+               _transfer(
                 msg.sender,
                 offerredAmounts[_offerNumber].offerrer,
                 balanceOf(msg.sender)
             );
+            
         }
     }
 
@@ -115,21 +123,5 @@ contract vault is ERC20Upgradeable, ERC721HolderUpgradeable, Ownable {
         );
     }
 
-    function _msgSender()
-        internal
-        view
-        override(ContextUpgradeable, Context)
-        returns (address)
-    {
-        return msg.sender;
-    }
-
-    function _msgData()
-        internal
-        view
-        override(ContextUpgradeable, Context)
-        returns (bytes calldata)
-    {
-        return msg.data;
-    }
+   
 }
