0ec87ba2b3d4f0528a8930cebc93e14e80c7a7d3
Gurleen Kaur <gurleen.kaur@antiersolutions.com>
2022-11-01 07:43:56 +0000

    updated

--- contracts/mock/USDT.sol
+++ contracts/mock/USDT.sol
@@ -5,27 +5,37 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "Interfaces/IUSDT.sol";
+import "hardhat/console.sol";
 
-contract Usd is IERC20, ERC20Burnable, Ownable {
+contract Usd is IERC20, ERC20Burnable, Ownable{
     constructor() ERC20("USDT", "USDT") {
-        _mint(msg.sender, 1000000 * 10**decimals());
+        
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
-        require(to != address(0), "Zero address sent");
+        require(to!=address(0),"Zero address sent");
         _mint(to, amount);
     }
 
-    function decimals() public view virtual override(ERC20) returns (uint8) {
-        return 18;
-    }
+   function decimals() public view virtual override(ERC20) returns (uint8) {
+		return 18;
+	}
 
-    function transfer(
-        address from,
-        address to,
-        uint256 _amount
-    ) external {
-        require(to != address(0), "Zero address sent");
+    function _transferFrom(address from, address to , uint256 _amount) external {
+        require(to!=address(0),"Zero address sent");
         transferFrom(from, to, _amount);
     }
+
+    function transfer(address from, address to , uint256 _amount) external {
+        require(to!=address(0),"Zero address sent");
+        console.log("from",from);
+                console.log("to",to);
+                        console.log("amount",_amount);
+
+
+        _transfer(from,to, _amount);
+    }
+
+
 }
