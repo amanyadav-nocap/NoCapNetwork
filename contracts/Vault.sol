f1120bbe709a9e0849aa85e7c924c96111a5e6dc
yashvardhan <yashvardhan@antiersolutions.com>
2022-11-07 18:28:11 +0530

    changes and updates

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -32,9 +32,10 @@ contract vault is
     struct offer {
         address offerrer;
         uint256 offerred;
-        uint256 paidAmount;
+        uint256 fractionAcquired;
     }
     mapping(uint256 => offer) private offerredAmounts;
+    mapping(address => bool) private excludeFee;
 
     function initialize(
         string memory _name,
@@ -76,14 +77,18 @@ contract vault is
         require(fractionSupply >= _fractionAmount, "NES"); //Not Enough Supply
         // IUSDT(usdt)._transferFrom(msg.sender, marketFeeWallet, (_fractionAmount*fractionPrice*1)/100);//Platform Fee
         uint256 amount = _fractionAmount * fractionPrice;
-        console.log("buy amount", amount);
-        IUSDT(usdt)._transferFrom(msg.sender, owner(), amount);
-        IUSDT(usdt).transfer(owner(), marketFeeWallet, (amount * 1) / 100);
-
+        IUSDT(usdt).transferFrom(
+            msg.sender,
+            marketFeeWallet,
+            (amount * 1) / 100
+        );
+        IUSDT(usdt).transferFrom(
+            msg.sender,
+            owner(),
+            (amount - ((amount * 1) / 100))
+        );
         _transfer(address(this), msg.sender, _fractionAmount);
         tokenAmount = tokenAmount + balanceOf(msg.sender);
-        console.log("t amount",tokenAmount);
-        console.log("balance amount", balanceOf(msg.sender));
         fractionSupply = fractionSupply - _fractionAmount;
         if (fractionSupply == 0) primaryBuyEnd = true;
     }
@@ -94,12 +99,13 @@ contract vault is
         uint256 _amount
     ) internal override(ERC20Upgradeable) {
         require(_to != address(0), "ZA"); //zero address
-        uint256 amountTransferred = _amount - ((_amount * 25) / 1000);
-        console.log("amountT", amountTransferred);
-        console.log("amount", _amount);
-        super._transfer(_from, taxWallet, (_amount * 25) / 1000); //TAX amount
-        super._transfer(_from, _to, amountTransferred);
-        
+        if (excludeFee[msg.sender]) {
+            super._transfer(_from, _to, _amount);
+        } else {
+            uint256 amountTransferred = _amount - ((_amount * 25) / 1000);
+            super._transfer(_from, taxWallet, (_amount * 25) / 1000); //TAX amount
+            super._transfer(_from, _to, amountTransferred);
+        }
     }
 
     // for make offer are we taking fraction price of nft price?
@@ -107,48 +113,50 @@ contract vault is
     function makeOffer(uint256 offerredPrice) external {
         require(primaryBuyEnd, "NAY"); //Not Available Yet
         require(offerredPrice > fractionPrice, "PL"); //Price too low
-        uint256 priceToPay = offerredPrice * totalSupply();
-        console.log("token Amount", tokenAmount);
-        console.log("price to pay", priceToPay);
-        IUSDT(usdt)._transferFrom(msg.sender, address(this), priceToPay);
-        console.log("vault balance", usdt.balanceOf(address(this)));
-        offerredAmounts[offerNumber] = offer(
+        IUSDT(usdt)._transferFrom(
             msg.sender,
-            offerredPrice,
-            priceToPay
+            address(this),
+            offerredPrice * totalSupply()
         );
-        offerrers.push(offerNumber);
+        console.log("vault balance", usdt.balanceOf(address(this)));
+        offerredAmounts[offerNumber].offerrer = msg.sender;
+        offerredAmounts[offerNumber].offerred = offerredPrice;
+        // offerrers.push(offerNumber);
         offerNumber++;
     }
 
-    function voteOffer(uint256 _offerNumber, bool vote) external {
-        require(balanceOf(msg.sender) > 0, "NF"); //No Fractions
+    function voteOffer(uint256 _offerNumber) external {
+        require(balanceOf(msg.sender) != 0, "AV"); //Already Voted
         require(msg.sender != offerredAmounts[_offerNumber].offerrer, "ONA"); //Offerrer not allowed
-        if (vote == true) {
-            IUSDT(usdt).transfer(
-                address(this),
-                msg.sender,
-                (balanceOf(msg.sender) * offerredAmounts[_offerNumber].offerred)
-            );
-            console.log("usdt b",IUSDT(usdt).balanceOf(msg.sender));
-            _transfer(
-                msg.sender,
-                offerredAmounts[_offerNumber].offerrer,
-                balanceOf(msg.sender)
-            );
-        }
+        offerredAmounts[_offerNumber].fractionAcquired =
+            offerredAmounts[_offerNumber].fractionAcquired +
+            balanceOf(msg.sender);
+        //      IUSDT(usdt).transfer(
+        //     address(this),
+        //     marketFeeWallet,
+        //     (offerredAmounts[_offerNumber].paidAmount * 1) / 100
+        // ); //Platform Fee
+        _transfer(
+            msg.sender,
+            offerredAmounts[_offerNumber].offerrer,
+            balanceOf(msg.sender)
+        );
+
+        IUSDT(usdt).transfer(
+            address(this),
+            msg.sender,
+            (balanceOf(msg.sender) * offerredAmounts[_offerNumber].offerred)
+        );
     }
 
     function claim(uint256 _offerNumber) external {
         require(msg.sender == offerredAmounts[_offerNumber].offerrer, "NO"); //Not Offerrer
-        console.log("token Amount",tokenAmount);
-        require(balanceOf(msg.sender) >= (tokenAmount * 51) / 100, "NE"); //Not Eligible
-        IUSDT(usdt).transfer(
-            address(this),
-            marketFeeWallet,
-            (offerredAmounts[_offerNumber].paidAmount * 1) / 100
-        ); //Platform Fee
-        offerredAmounts[_offerNumber].paidAmount = 0;
+        console.log("token Amount", tokenAmount);
+        require(
+            offerredAmounts[_offerNumber].fractionAcquired >=
+                (tokenAmount * 51) / 100,
+            "NE"
+        ); //Not Eligible
         NFTSold = true;
         _transfer(msg.sender, address(this), balanceOf(msg.sender));
         IERC721Upgradeable(token721).safeTransferFrom(
@@ -157,30 +165,28 @@ contract vault is
             tokenID
         );
         finalOfferredAmount = offerredAmounts[_offerNumber].offerred;
-        bulkTransfer(offerrers);
     }
 
     function claimShare() external {
         require(NFTSold == true, "NFT not sold");
-        require(balanceOf(msg.sender) != 0, "AC"); //Already Claimed
-        console.log("fbalance",balanceOf(msg.sender));
+        
         uint256 _amount = balanceOf(msg.sender) * finalOfferredAmount;
-        // console.log("amount to pay",balanceOf(msg.sender));
-        console.log("amount to pay",_amount);
         _transfer(msg.sender, address(this), balanceOf(msg.sender));
-        console.log("amount to pay",_amount);
-        console.log("vault balance",IUSDT(usdt).balanceOf(address(this)));
         IUSDT(usdt).transfer(address(this), msg.sender, _amount);
-        console.log("vault balance",IUSDT(usdt).balanceOf(address(this)));
     }
 
-    function bulkTransfer(uint256[] memory _offerrers) internal {
-        for (uint256 i = 0; i < _offerrers.length; i++) {
-            IUSDT(usdt).transfer(
-                address(this),
-                offerredAmounts[_offerrers[i]].offerrer,
-                offerredAmounts[_offerrers[i]].paidAmount
-            );
-        }
+    function excludeFromFee(address _account, bool _toExclude) external {
+        require(_account != address(0), "ZA"); //Zero Address
+        excludeFee[_account] = _toExclude;
+    }
+
+    function claimOfferAmount(uint256 _offerNumber) external {
+        uint256 transferAmount = offerredAmounts[_offerNumber]
+            .fractionAcquired * offerredAmounts[_offerNumber].offerred;
+        IUSDT(usdt).transfer(
+            address(this),
+            offerredAmounts[_offerNumber].offerrer,
+            transferAmount
+        );
     }
 }
